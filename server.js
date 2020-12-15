2b4b729157d3b63d29dd2f9e5b1867ff1696395d
gareth0712 <gareth_0712@hotmail.com>
2020-12-15 21:47:59 +0800

    Finalize server.js before refractoring

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const server = require('net').createServer();
 const dotenv = require('dotenv');
-const moment = require('moment');
 
 const broadcast = require('./utils/broadcast');
 const logger = require('./utils/logger');
-const {
-  addMessages,
-  showHistoricalMessages,
-} = require('./utils/handle-messages');
-const { doesNameExist, getNicknames } = require('./utils/validator');
+const { addMessages, showHistoricalMessages } = require('./utils/handle-messages');
+const { doesNameExist, getNicknames, isValidName } = require('./utils/validator');
+const { messageWithTime } = require('./utils/time');
 
 dotenv.config({ path: './config.env' });
 
@@ -29,60 +26,50 @@ server.on('connection', (socket) => {
   id += 1;
   socket.id = id;
 
-  logger.info(
-    `New connection from ${socket.remoteAddress}:${socket.remotePort} establishes to the chat server`
-  );
+  logger.info(`New connection from ${socket.remoteAddress}:${socket.remotePort} establishes to the chat server`);
   logger.info(`ID ${socket.id} is assigned to the new joiner`);
 
   // Greet the user
   socket.write('< Welcome to my chat server! What is your nickname?\n');
-  socket.write('\u0007');
 
   // Input listener
   socket.on('data', (data) => {
     // User Exit server
     const dataStr = data.toString().trim();
     if (dataStr === '.exit') {
-      socket.end();
+      return socket.end();
     }
-
-    const time = moment().format('hh:mm:ss');
-    let message = `< [${time}] `;
 
     // Request user to enter a nickname
     if (socket.nickname === undefined) {
+      // Check if name contains special characters or space
+      if (!isValidName(dataStr)) {
+        return socket.write(`Empty name, spaces and special characters are not allowed for nickname, except '-' and '_'.\n`);
+      }
       // Check if the name is taken
       if (!doesNameExist(dataStr, sockets)) {
         socket.nickname = dataStr;
-        logger.info(
-          `User with ID ${socket.id} has taken a nickname "${socket.nickname}"`
-        );
+        logger.info(`User with ID ${socket.id} has taken a nickname "${socket.nickname}"`);
 
         // Let user know the existing users in chat server
         if (sockets.length !== 0) {
           const nicknames = getNicknames(sockets);
-          socket.write(
-            `You are connected with ${
-              nicknames.length
-            } other users: [${nicknames.join(', ')}]\n`
-          );
+          socket.write(`You are connected with ${nicknames.length} ${nicknames.length > 1 ? 'other users' : 'user'}: [${nicknames.join(', ')}]\n`);
         } else {
-          socket.write(`You are the first joiner of the chat server!\n`);
+          socket.write(`${messageWithTime()} *You are the first joiner of the chat server!*\n`);
         }
 
         showHistoricalMessages(socket, historicalMessages);
 
         // Broadcast to existing users of new joiner
         sockets.push(socket);
-        message += `*${socket.nickname} has joined the chat*\n`;
+        const message = `${messageWithTime()} *<${socket.nickname}> has joined the chat*\n`;
         return broadcast(socket.nickname, sockets, message);
       }
-      return socket.write(
-        'Your desired name has already been taken. Please choose another name.\n'
-      );
+      return socket.write('Your desired nickname has already been taken. Please choose another name.\n');
     }
 
-    message += `<${socket.nickname}> ${dataStr}\n`;
+    const message = `${messageWithTime()} <${socket.nickname}> ${dataStr}\n`;
     addMessages(message, historicalMessages);
     return broadcast(socket.nickname, sockets, message);
   });
@@ -90,14 +77,8 @@ server.on('connection', (socket) => {
   // Socket End listener
   socket.on('end', () => {
     removeSocket(socket);
-    logger.info(
-      `User with nickname ${socket.nickname} of ID ${socket.id} disconnected from server`
-    );
-    broadcast(
-      socket.nickname,
-      sockets,
-      `${socket.nickname} left the chat server.\n`
-    );
+    logger.info(`User with nickname ${socket.nickname} of ID ${socket.id} disconnected from server`);
+    broadcast(socket.nickname, sockets, `${messageWithTime()} *<${socket.nickname}> left the chat server.*\n`);
   });
 
   // Socket Error listener
