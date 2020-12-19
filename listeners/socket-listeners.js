9caf71fc02cda494ad4a9d448b8d3ef99f9957d3
gareth0712 <gareth_0712@hotmail.com>
2020-12-20 00:07:07 +0800

    rename socket-listeners and update message wordings

--- listeners/socket-listeners.js
+++ listeners/socket-listeners.js
@@ -24,17 +24,17 @@ const userInputListener = (socket) => {
     if (socket.nickname === undefined) {
       // Check if name contains special characters or space
       if (!isValidName(dataStr)) {
-        return socket.write(`Empty name, spaces and special characters are not allowed for nickname, except '-' and '_'.\n`);
+        return socket.write(`< Empty name, spaces and special characters are not allowed for nickname, except '-' and '_'.\n`);
       }
       // Check if the name is taken
       if (!doesNameExist(dataStr, sockets)) {
         socket.nickname = dataStr;
-        logger.info(`User with ID ${socket.id} has taken a nickname "${socket.nickname}"`);
+        logger.info(`< User with ID ${socket.id} has taken a nickname "${socket.nickname}"`);
 
         // Let user know the existing users in chat server
         if (sockets.length !== 0) {
           const nicknames = getNicknames(sockets);
-          socket.write(`You are connected with ${nicknames.length} ${nicknames.length > 1 ? 'other users' : 'user'}: [${nicknames.join(', ')}]\n`);
+          socket.write(`< You are connected with ${nicknames.length} ${nicknames.length > 1 ? 'other users' : 'user'}: [${nicknames.join(', ')}]\n`);
         } else {
           socket.write(`${messageWithTime()} *You are the first joiner of the chat server!*\n`);
         }
@@ -46,7 +46,7 @@ const userInputListener = (socket) => {
         const message = `${messageWithTime()} *<${socket.nickname}> has joined the chat*\n`;
         return broadcast(socket.nickname, sockets, message);
       }
-      return socket.write('Your desired nickname has already been taken. Please choose another name.\n');
+      return socket.write('< Your desired nickname has already been taken. Please choose another name.\n');
     }
 
     const message = `${messageWithTime()} <${socket.nickname}> ${dataStr}\n`;
@@ -59,13 +59,13 @@ const endListener = (socket) => {
   socket.on('end', () => {
     removeSocket(socket);
     logger.info(`User with nickname ${socket.nickname} of ID ${socket.id} disconnected from server`);
-    broadcast(socket.nickname, sockets, `${messageWithTime()} *<${socket.nickname}> left the chat server.*\n`);
+    broadcast(socket.nickname, sockets, `${messageWithTime()} *<${socket.nickname}> left the chat server*\n`);
   });
 };
 
 const errorListener = (socket) => {
   socket.on('error', (err) => {
-    socket.write('Error occurred in the chat server.');
+    socket.write('Error occurred in the chat server');
     logger.error(`error occurred: ${err.message}`);
     logger.error(err);
   });
